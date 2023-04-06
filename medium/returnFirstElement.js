c88a3a21c990f409ee5ec4c8554d689082a46637
Giftos1 <giftsydney676@gmail.com>
2023-04-06 14:46:24 +1000

    return first element of array

---
+++ medium/returnFirstElement.js
@@ -0,0 +1,7 @@
+// this function takes an array containing only numbers and returns the first element
+
+function getFirstValue(arr) {
+    return arr.shift();
+}
+
+console.log(getFirstValue([1, 2, 3]))
