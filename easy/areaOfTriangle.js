2bc4ac5086daac2d5ae4ad9615db9d5b1a23c438
Giftos1 <giftsydney676@gmail.com>
2023-04-06 14:32:18 +1000

    get triangle area

---
+++ easy/areaOfTriangle.js
@@ -0,0 +1,7 @@
+// this function that takes the base and height of a triangle and return its area
+
+function triArea(base, height) {
+    return 0.5 * base * height;
+}
+
+console.log(triArea(10, 10));
