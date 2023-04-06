7daa715a2e0a45c9ac85cd4d27958804570b9c9b
Giftos1 <giftsydney676@gmail.com>
2023-04-06 14:55:35 +1000

    calculate age in days

---
+++ medium/ageToDays.js
@@ -0,0 +1,9 @@
+// the function takes the age in years and returns the age in days
+// use 364 days as length of years
+// ignore leap years and days between last birthday and now.
+
+function calcAge(age) {
+    return age * 364;
+}
+
+console.log(calcAge(10));
