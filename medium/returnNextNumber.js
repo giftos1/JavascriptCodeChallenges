e767a563cba617c5ffbeaf4d63cb92f0c18425ec
Giftos1 <giftsydney676@gmail.com>
2023-04-07 11:28:20 +1000

    increase number by 1

---
+++ medium/returnNextNumber.js
@@ -0,0 +1,7 @@
+// This function takes a number as an argument, increments the number by +1 and returns the result
+
+function addition(number) {
+    return number + 1;
+}
+
+console.log(addition(4));
