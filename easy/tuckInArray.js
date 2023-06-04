1fc51c5252b0ebff7432081ba84ea949e4aacf25
Giftos1 <giftsydney676@gmail.com>
2023-06-04 21:54:39 +1000

    include floor method

--- easy/tuckInArray.js
+++ easy/tuckInArray.js
@@ -4,7 +4,7 @@ let couples = ["Joy", "Gift", "Ogingo", "Sydo"];
 let notCouples = ["Shiro", "Shonko"];
 
 function tuckIn(array1, array2) {
-    let middleOfArray = (array1.length / 2);
+    let middleOfArray = Math.floor((array1.length / 2));
     // return array1.slice().concat(array2).split(",");
     return array1.slice(0, middleOfArray).concat(array2).concat(array1.slice(middleOfArray));
 }
