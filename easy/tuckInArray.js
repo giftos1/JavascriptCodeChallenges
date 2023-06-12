eb6fe3a842cb575db7239990c7c4f3ffebc5d335
Giftos1 <giftsydney676@gmail.com>
2023-06-12 14:04:34 +1000

    insert array in the middle of an array

--- easy/tuckInArray.js
+++ easy/tuckInArray.js
@@ -5,7 +5,6 @@ let notCouples = ["Shiro", "Shonko"];
 
 function tuckIn(array1, array2) {
     let middleOfArray = Math.floor((array1.length / 2));
-    // return array1.slice().concat(array2).split(",");
     return array1.slice(0, middleOfArray).concat(array2).concat(array1.slice(middleOfArray));
 }
 
