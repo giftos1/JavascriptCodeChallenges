a05ff424dfa2f5d16065b879683db1944788c89f
Giftos1 <giftsydney676@gmail.com>
2023-06-12 14:32:23 +1000

    check if object is empty

--- medium/convertObjectToArray.js
+++ medium/convertObjectToArray.js
@@ -2,8 +2,12 @@
  // a key-value pair in the form of an array
 
  function toArray(obj) {
+    if (Object.keys(obj).length === 0) {
+        return [];
+    }
      return Object.entries(obj);
 
  }
 
  console.log(toArray({ a: 1, b: 2 }))
+ console.log(toArray({}))
