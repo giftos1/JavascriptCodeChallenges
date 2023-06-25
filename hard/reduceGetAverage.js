175e9983d1e3b78e92f0283e365078690ba6e9b1
Giftos1 <giftsydney676@gmail.com>
2023-06-25 22:49:32 +1000

    get average ChrisN movie ratings

--- hard/reduceGetAverage.js
+++ hard/reduceGetAverage.js
@@ -2,7 +2,7 @@
 The variable watchList holds an array of objects with information on several movies.
 Use reduce to find the average IMDB rating of the movies directed by Christopher Nolan.
 The function was meant to not use any for loops
-You can user Number to convert the string imdbRatings to number in stead of the positive sign
+You can user Number to convert the string imdbRatings to number instead of the positive sign
 */
 
 const watchList = [
