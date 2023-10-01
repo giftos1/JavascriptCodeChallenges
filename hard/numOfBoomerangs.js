72a89ae31dcb9f6918cb43dccf9bdb014245ff33
Giftos1 <giftsydney676@gmail.com>
2023-10-02 08:42:19 +1000

    Revert "return true if integer is pandigital"
    This reverts commit 3f9bc9c877a0f4c7f16bf42264344027a7b39552.

--- hard/numOfBoomerangs.js
+++
@@ -1,4 +0,0 @@
- // A boomerang is a V-shaped sequence that is either upright or upside down. Specifically,
- // a boomerang can be defined as: sub-array of length 3, with the first and last digits
- // being the same and the middle digit being different.
-
