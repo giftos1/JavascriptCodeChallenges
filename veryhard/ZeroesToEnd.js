aecc27988949fcccc984930da0b830f326ff35be
Gifto <69237210+giftos1@users.noreply.github.com>
2023-11-23 17:41:11 +1000

    move all zeroes to the end of the array

---
+++ veryhard/ZeroesToEnd.js
@@ -0,0 +1,28 @@
+/*The function moves all the zeroes to the end of an array.
+* I returned a copy of the original array*/
+
+
+function zeroesToEnd(arr) {
+    let newArr = [...arr];
+
+    function allZero(element) {
+        return element === 0;
+    }
+
+    if (!arr.includes(0) || arr.every(allZero) ) {
+        return arr;
+    }
+        for (let i = 0; i < arr.length; i++) {
+            if (arr[i] === 0) {
+                newArr.splice(newArr.indexOf(arr[i]), 1);
+                newArr.push(arr[i]);
+            }
+        }
+    arr = newArr;
+    return arr;
+}
+
+console.log(zeroesToEnd([1, 2, 0, 0, 4, 0, 5])); // [1, 2, 4, 5, 0, 0, 0]
+console.log(zeroesToEnd([0, 0, 2, 0, 5])); // [2, 5, 0, 0, 0]
+console.log(zeroesToEnd([4, 4, 5])); // [4, 4, 5]
+console.log(zeroesToEnd([0, 0])); // [0, 0]
